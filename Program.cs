cba1022616f1b0fdcd553fbfa458aa6e1eaf046a
Will <wilton007@hotmail.com>
2023-10-21 22:43:30 -0300

    jogo da velha

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using jogoDaVelha;
+bool vf = true;
+bool vf2 = true;
+bool vencedor = true;
+Service service = new Service();
+Random aleatorio = new Random();
+
+        Console.Write("digiete seu nome: ");
+        string jogador1 = Console.ReadLine();
+        Console.Clear();
+        Console.WriteLine($"{jogador1} vs RoboTron");
+        int cont = 0;
+        while (vencedor)
+        {
+            Console.Clear();
+            int n1 = 0;
+            service.game();
+            while (vf2)
+            {
+                Console.Write($"{jogador1} sua vez: ");
+                n1 = Convert.ToInt32(Console.ReadLine());
+                if (service.jogar(n1, "x"))
+                {
+                    vf2 = false;
+                }
+            }
+
+            if (service.venc("x"))
+            {
+                Console.Clear();
+                Console.WriteLine();
+                Console.WriteLine($"{jogador1} venceu");
+                service.game();
+                break;
+            }
+
+
+            Console.WriteLine();
+            int empate = 0;
+            while (vf)
+            {
+                n1 = Convert.ToInt32(aleatorio.Next(0, 8));
+                vf = !service.jogar(n1, "O");
+                if (empate > 10)
+                {
+                    Console.WriteLine("empate");
+                    vencedor = false;
+                    break;
+                }
+                empate++;
+
+            }
+            if (service.venc("O"))
+            {
+                Console.Clear();
+                Console.WriteLine();
+                Console.WriteLine($"Robotron venceu");
+                service.game();
+                break;
+            }
+
+            if (cont == 5)
+            {
+                Console.WriteLine("empate");
+                vencedor = false;
+                break;
+            }
+            cont++;
+
+            vf = true;
+            vf2 = true;
+
+        }
+
+        
---
+++ Service.cs
@@ -0,0 +1,122 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jogoDaVelha
+{
+    public class Service
+    {
+        string[,] jogo = { { "0", "1", "2" }, { "3", "4", "5" }, { "6", "7", "8" } };
+
+        public void game()
+        {
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(jogo[i, j] + " |");
+
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private bool verificar(string n1)
+        {
+            bool vf = true;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (jogo[i, j] == n1)
+                    {
+                        vf = true;
+                        i=3;
+                        break;
+                    }
+                    else
+                    {
+                        vf = false;
+                    }
+
+                }
+               
+            }
+            return vf;
+        }
+
+
+        public bool jogar(int numero, string jogador)
+        {
+
+            string n1 = Convert.ToString(numero);
+            if (verificar(n1))
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        if (jogo[i, j] == n1)
+                        {
+                            jogo[i, j] = jogador;
+                            i =3;
+                            break;
+                        }
+                    }
+                }
+                return true;
+                
+            }
+            else
+            {
+                return false;
+                
+            }
+        }
+
+        public bool venc(string jogador)
+        {
+            if (jogo[0, 0] == jogador && jogo[0, 1] == jogador && jogo[0, 2] == jogador)
+            {
+                return true;
+            }
+            else if (jogo[1, 0] == jogador && jogo[1, 1] == jogador && jogo[1, 2] == jogador)
+            {
+                return true;
+            }
+            else if (jogo[2, 0] == jogador && jogo[2, 1] == jogador && jogo[2, 2] == jogador)
+            {
+                return true;
+            }
+            else if (jogo[0, 0] == jogador && jogo[1, 0] == jogador && jogo[2, 0] == jogador)
+            {
+                return true;
+            }
+            else if (jogo[0, 1] == jogador && jogo[1, 1] == jogador && jogo[2, 1] == jogador)
+            {
+                return true;
+            }
+            else if (jogo[0, 2] == jogador && jogo[1, 2] == jogador && jogo[2, 2] == jogador)
+            {
+                return true;
+            }
+            else if (jogo[0, 0] == jogador && jogo[1, 1] == jogador && jogo[2, 2] == jogador)
+            {
+                return true;
+            }
+            else if (jogo[0, 2] == jogador && jogo[1, 1] == jogador && jogo[2, 0] == jogador)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+    }
+
+}
